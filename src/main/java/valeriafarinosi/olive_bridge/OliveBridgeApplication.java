0ce1b65e345ceb6c08e66ea7a740f1deab4e08fb
Vals098 <valeria.farinosi@gmail.com>
2026-08-14 17:59:45 +0200

    Initial commit

---
+++ src/main/java/valeriafarinosi/olive_bridge/OliveBridgeApplication.java
@@ -0,0 +1,13 @@
+package valeriafarinosi.olive_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/valeriafarinosi/olive_bridge/OliveBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package valeriafarinosi.olive_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
