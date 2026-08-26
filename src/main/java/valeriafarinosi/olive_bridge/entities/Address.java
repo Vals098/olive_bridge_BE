4fd3374c8fb47ba0efdc13243a07321eb92925ef
Vals098 <valeria.farinosi@gmail.com>
2026-08-26 22:47:21 +0200

    feat: add Address entity

---
+++ src/main/java/valeriafarinosi/olive_bridge/entities/Address.java
@@ -0,0 +1,67 @@
+package valeriafarinosi.olive_bridge.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "addresses")
+@NoArgsConstructor
+@Getter
+@ToString
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID addressId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String label;
+
+    @Column(nullable = false)
+    private String recipientName;
+
+    @Column(nullable = false)
+    private String postalCode;
+
+    @Column(nullable = false)
+    private String prefecture;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String area;
+
+    @Column(nullable = false)
+    private String street;
+
+    private String building;
+
+    public Address(
+            User user,
+            String label,
+            String recipientName,
+            String postalCode,
+            String prefecture,
+            String city,
+            String area,
+            String street,
+            String building
+    ) {
+        this.user = user;
+        this.label = label;
+        this.recipientName = recipientName;
+        this.postalCode = postalCode;
+        this.prefecture = prefecture;
+        this.city = city;
+        this.area = area;
+        this.street = street;
+        this.building = building;
+    }
+}
