df5cd7b1cc8e3e0cbeba3d7bb3f6b6f3ea35441b
Vals098 <valeria.farinosi@gmail.com>
2026-09-03 23:29:14 +0200

    feat: add product detail endpoint

--- src/main/java/valeriafarinosi/olive_bridge/controllers/ProductController.java
+++ src/main/java/valeriafarinosi/olive_bridge/controllers/ProductController.java
@@ -1,12 +1,14 @@
 package valeriafarinosi.olive_bridge.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import valeriafarinosi.olive_bridge.entities.Product;
 import valeriafarinosi.olive_bridge.services.ProductService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -21,5 +23,10 @@ public class ProductController {
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
+    }
+
+    @GetMapping("/{productId}")
+    public Product findById(@PathVariable UUID productId) {
+        return productService.findById(productId);
     }
 }
--- src/main/java/valeriafarinosi/olive_bridge/services/ProductService.java
+++ src/main/java/valeriafarinosi/olive_bridge/services/ProductService.java
@@ -2,9 +2,11 @@ package valeriafarinosi.olive_bridge.services;
 
 import org.springframework.stereotype.Service;
 import valeriafarinosi.olive_bridge.entities.Product;
+import valeriafarinosi.olive_bridge.exceptions.NotFoundException;
 import valeriafarinosi.olive_bridge.repositories.ProductRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductService {
@@ -17,5 +19,10 @@ public class ProductService {
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    public Product findById(UUID productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new NotFoundException("Product not found"));
     }
 }
